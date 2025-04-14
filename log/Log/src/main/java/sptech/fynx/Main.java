ff33b9717a2e26f965871cddddf1d660f90aad26
giomafra <giovanna.msilva@sptech.school>
2025-04-13 21:59:16 -0300

    jar configurado pronto
    # Conflicts:
    #	log/Log/banco-de-dados.mv.db
    #	log/Log/pom.xml
    #	log/Log/src/main/java/sptech/fynx/LeitorExcel.java
    #	log/Log/src/main/java/sptech/fynx/Main.java
    #	log/Log/src/main/java/sptech/fynx/dao/ConexaoBD.java
    #	log/Log/src/main/java/sptech/fynx/dao/DadosDAO.java

--- log/Log/src/main/java/sptech/fynx/Main.java
+++ log/Log/src/main/java/sptech/fynx/Main.java
@@ -2,36 +2,21 @@ package sptech.fynx;
 
 import sptech.fynx.dao.ConexaoBD;
 import sptech.fynx.dao.DadosDAO;
+import sptech.fynx.dao.LogDAO;
 
 public class Main {
     public static void main(String[] args) {
-/*
-        String[] tarefas = {"Processo 1", "Processo 2", "Processo 3"};
-        Scanner scanner = new Scanner(System.in);
+        String caminhoArquivo = "C:\\Users\\giopa\\Downloads\\tratamento_dados.xlsx";
 
-        sptech.fynx.dao.Log.generateLog(tarefas);
+        // Conectar ao banco
+        ConexaoBD conexaoBD = new ConexaoBD();
 
-        System.out.print("\nDeseja consultar algum processo específico? (s/n): ");
-        String resposta = scanner.nextLine();
+        // Criação do objeto DadosDAO e LogDAO já com o DataSource
+        DadosDAO dadosDAO = new DadosDAO(conexaoBD.getConexaoBD());
+        LogDAO logDAO = new LogDAO(conexaoBD.getConexaoBD());
 
-        if (resposta.equalsIgnoreCase("s")) {
-            System.out.print("Digite o nome ou parte do nome do processo: ");
-            String processo = scanner.nextLine();
-            sptech.fynx.dao.Log.consultarLog(processo);
-        }
-
-        scanner.close();
-
-        
-*/
-
-
-        String caminhoArquivo = "C:\\Users\\Giovanna\\Downloads\\tratamento_dados.xlsx";
-
+        // Ler a planilha e inserir os dados no banco
         LeitorExcel leitor = new LeitorExcel();
-        leitor.lerPlanilha(caminhoArquivo);
-
-        ConexaoBD conexao = new ConexaoBD();
-        DadosDAO dadosDAO = new DadosDAO(conexao.getConexaoBD());
+        leitor.lerPlanilha(caminhoArquivo, dadosDAO, logDAO);  // Passando logDAO como argumento
     }
 }
