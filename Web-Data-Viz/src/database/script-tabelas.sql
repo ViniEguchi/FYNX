cf3caabf188234248b6929298d7e6af187bdbc84
ViniEguchi <vinicius.eguchi@sptech.school>
2025-04-16 23:45:58 -0300

    fix: tabela log campo nome > descricao

--- Web-Data-Viz/src/database/script-tabelas.sql
+++ Web-Data-Viz/src/database/script-tabelas.sql
@@ -97,7 +97,7 @@ CREATE TABLE IF NOT EXISTS log (
 	idLog INT PRIMARY KEY AUTO_INCREMENT,
 	data_hora_inicio DATETIME,
 	data_hora_fim DATETIME,
-	nome VARCHAR(50),
+	descricao VARCHAR(100),
 	status_log BOOLEAN,
 	erro VARCHAR(50)
 );
