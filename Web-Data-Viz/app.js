a459178eb5dac13d2d53d3d850c1932b20b3636b
ViniEguchi <vinicius.eguchi@sptech.school>
2025-05-10 08:08:26 -0300

    Formulário Dinâmico, falta validações (#92)
    * feat: adicionando .env.dev no gitignore
    
    * feat: adicionando formulario no app.js e criando routes/formulario
    
    * feat: formulario funcionando

--- Web-Data-Viz/app.js
+++ Web-Data-Viz/app.js
@@ -23,6 +23,7 @@ var aquariosRouter = require("./src/routes/aquarios");
 var empresasRouter = require("./src/routes/empresas");
 var funcionariosRouter = require("./src/routes/funcionarios");
 var adminRouter = require("./src/routes/admin");
+var formularioRouter = require("./src/routes/formulario")
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
@@ -38,6 +39,7 @@ app.use("/aquarios", aquariosRouter);
 app.use("/empresas", empresasRouter);
 app.use("/funcionarios", funcionariosRouter);
 app.use("/admin", adminRouter);
+app.use("/formulario", formularioRouter)
 
 app.listen(PORTA_APP, function () {
     console.log(`
