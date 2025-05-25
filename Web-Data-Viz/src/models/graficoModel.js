23ad5a58a33ffc6ed5b40326946d9f48ec6d3d53
ViniEguchi <vinicius.eguchi@sptech.school>
2025-05-25 01:09:05 -0300

    fix: corrigindo a conta de uma kpi

--- Web-Data-Viz/src/models/graficoModel.js
+++ Web-Data-Viz/src/models/graficoModel.js
@@ -14,7 +14,7 @@ function exibirKpiDash(periodo, setor) {
     var instrucaoSql = `
         SELECT
         (SELECT AVG(valor_operacao * (1 + juros)) FROM historico WHERE data_contratacao >= DATE_SUB('2024-11-24', INTERVAL ${periodo} MONTH) AND WHERE subsetor_cnae = ${setor}) AS mediaOperacoes,
-        (SELECT SUM(valor_operacao * (1 + juros)) FROM historico WHERE data_contratacao >= DATE_SUB('2024-11-24', INTERVAL ${periodo} MONTH) AND WHERE subsetor_cnae = ${setor}) AS somaCredito,
+        (SELECT SUM(valor_operacao * (1 + (juros / 100))) FROM historico WHERE data_contratacao >= DATE_SUB('2024-11-24', INTERVAL ${periodo} MONTH) AND WHERE subsetor_cnae = ${setor}) AS somaCredito,
         (SELECT * FROM historico WHERE data_contratacao >= DATE_SUB('2024-11-24', INTERVAL ${periodo} MONTH) AND WHERE subsetor_cnae = ${setor}) AS variacaoCredito,
         FROM historico;
     `;
