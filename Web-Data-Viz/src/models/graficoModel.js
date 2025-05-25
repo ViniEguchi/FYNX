f7b08c0ddb50652b3ed6a698da33ccafbad02460
ViniEguchi <vinicius.eguchi@sptech.school>
2025-05-24 22:40:01 -0300

    feat: configuração inicial das kpis da tela dashboard

--- Web-Data-Viz/src/models/graficoModel.js
+++ Web-Data-Viz/src/models/graficoModel.js
@@ -1,6 +1,6 @@
 var database = require("../database/config")
 
-function preencherSetores(nome, email, mensagem) {
+function preencherSetores() {
     console.log("ACESSEI O empresa  MODEL \n \n\t\t >> Se aqui der erro de 'Error: connect ECONNREFUSED',\n \t\t >> verifique suas credenciais de acesso ao banco\n \t\t >> e se o servidor de seu BD está rodando corretamente. \n\n function buscarPorId()");
 
     var instrucaoSql = `SELECT DISTINCT subsetor_cnae FROM historico;`;
@@ -10,6 +10,21 @@ function preencherSetores(nome, email, mensagem) {
     return database.executar(instrucaoSql);
 }
 
+function exibirKpiDash(periodo, setor) {
+    var instrucaoSql = `
+        SELECT
+        (SELECT * FROM historico WHERE data_venda >= DATE_SUB('2024-11-24', INTERVAL ${periodo} MONTH) AND WHERE subsetor_cnae = ${setor}) AS mediaOperacoes,
+        (SELECT SUM(valor_operacao * (1 + juros)) FROM historico WHERE data_venda >= DATE_SUB('2024-11-24', INTERVAL ${periodo} MONTH) AND WHERE subsetor_cnae = ${setor}) AS somaCredito,
+        (SELECT * FROM historico WHERE data_venda >= DATE_SUB('2024-11-24', INTERVAL ${periodo} MONTH) AND WHERE subsetor_cnae = ${setor}) AS variacaoCredito,
+        FROM historico;
+    `;
+
+    console.log("Executando a instrução SQL: \n" + instrucaoSql);
+
+    return database.executar(instrucaoSql);
+}
+
 module.exports = {
-    preencherSetores
+    preencherSetores,
+    exibirKpiDash
 }
