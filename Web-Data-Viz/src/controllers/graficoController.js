298809e7942a090bacb8171f8fc22c7d4ba5a63f
Guilherme Serafim <guilherme.lira@sptech.school>
2025-05-26 17:09:58 -0300

    feat: graficos dinamicos e novas variaveis do .env shell (#120)

--- Web-Data-Viz/src/controllers/graficoController.js
+++ Web-Data-Viz/src/controllers/graficoController.js
@@ -14,6 +14,62 @@ function preencherSetores(req, res) {
 
 }
 
+function totalOperacoes(req, res) {
+    console.log("função totalOperacoes");
+
+    graficoModel.totalOperacoes()
+        .then(function (resultado) {
+            res.json(resultado);
+        })
+        .catch(function (erro) {
+            console.error("Erro ao buscar total de operações:", erro);
+            res.status(500).json(erro.sqlMessage || erro.message);
+        });
+
+}
+
+function jurosMedioSetor(req, res) {
+    console.log("função jurosMedioSetor");
+
+    graficoModel.jurosMedioSetor()
+        .then(function (resultado) {
+            res.json(resultado);
+        })
+        .catch(function (erro) {
+            console.error("Erro ao buscar total de operações:", erro);
+            res.status(500).json(erro.sqlMessage || erro.message);
+        });
+
+}
+
+function prazoAmortizacaoMes(req, res) {
+    console.log("função jurosMedioSetor");
+
+    graficoModel.prazoAmortizacaoMes()
+        .then(function (resultado) {
+            res.json(resultado);
+        })
+        .catch(function (erro) {
+            console.error("Erro ao buscar prazo de amortização:", erro);
+            res.status(500).json(erro.sqlMessage || erro.message);
+        });
+
+}
+
+function valorOperacoesMes(req, res) {
+    console.log("função valorOperacoesMes");
+
+    graficoModel.valorOperacoesMes()
+        .then(function (resultado) {
+            res.json(resultado);
+        })
+        .catch(function (erro) {
+            console.error("Erro ao buscar valor das operações:", erro);
+            res.status(500).json(erro.sqlMessage || erro.message);
+        });
+
+}
+
 function exibirKpiDash(req, res) {
     var periodo = req.params.periodo;
     var setor = req.params.setor;
@@ -33,5 +89,10 @@ function exibirKpiDash(req, res) {
 
 module.exports = {
     preencherSetores,
-    exibirKpiDash
+    exibirKpiDash,
+    preencherSetores,
+    totalOperacoes,
+    jurosMedioSetor,
+    prazoAmortizacaoMes,
+    valorOperacoesMes
 }
--- Web-Data-Viz/src/routes/grafico.js
+++ Web-Data-Viz/src/routes/grafico.js
@@ -7,8 +7,25 @@ router.get("/preencherSetores", function (req, res) {
   graficoController.preencherSetores(req, res);
 });
 
+router.get("/totalOperacoes", function (req, res) {
+  graficoController.totalOperacoes(req, res);
+});
+
+router.get("/jurosMedioSetor", function (req, res) {
+  graficoController.jurosMedioSetor(req, res);
+});
+
+router.get("/prazoAmortizacaoMes", function (req, res) {
+  graficoController.prazoAmortizacaoMes(req, res);
+});
+
+router.get("/valorOperacoesMes", function (req, res) {
+  graficoController.valorOperacoesMes(req, res);
+});
+
 router.get("/exibirKpiDash/:periodo/:setor", function (req, res) {
   graficoController.exibirKpiDash(req, res);
 });
+
 
 module.exports = router;
